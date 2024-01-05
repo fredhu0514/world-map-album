f1271008eb243c4fa28914f18e2aeecbecaa5390
zhuminxuan <zhuminxuan0521@126.com>
2024-01-04 23:11:32 -0800

    put method

--- my-travel-map/src/app/api/blog/route.js
+++ my-travel-map/src/app/api/blog/route.js
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 import {
    initializeDb,
    deleteBlog,
-   addBlog 
+   updateBlog,
+   addBlog
 } from "@/database/database";
 
 export const DELETE = async (req, res) => {
@@ -32,6 +33,35 @@ export const POST = async (req, res) => {
         console.log(err);
         return NextResponse.json(
             { message: "Failed to create a new blog post." },
+            { status: 500 }
+        );
+    }
+ };
+
+ export const PUT = async (req, res) => {
+    try {
+        await initializeDb();
+        const { id, title, description } = await req.json();
+        if (!id) {
+            return NextResponse.json(
+                { message: "No pid provided for updating" },
+                { status: 400 }
+            );
+        }
+        console.log(`UPDATE BLOG POST WITH ID: ${id}`);
+        const result = await updateBlog(id, title, description);
+        if (result.updated) {
+            return NextResponse.json({ message: "Blog post updated successfully" }, { status: 200 });
+        } else {
+            return NextResponse.json(
+                { message: "Blog post not found or no changes made" },
+                { status: 404 }
+            );
+        }
+    } catch (err) {
+        console.log(err);
+        return NextResponse.json(
+            { message: "Failed to update the blog post." },
             { status: 500 }
         );
     }
