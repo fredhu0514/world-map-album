9429036c02c863112ddf2f57736aee596df0c1b2
Fred Hu <65211410+fredhu0514@users.noreply.github.com>
2024-01-05 00:58:00 -0500

    Update route.js

--- my-travel-map/src/app/api/blog/route.js
+++ my-travel-map/src/app/api/blog/route.js
@@ -3,13 +3,14 @@ import {
    initializeDb,
    deleteBlog,
 } from "@/database/database";
+
 export const DELETE = async (req, res) => {
     try {
         await initializeDb();
         console.log("DELETE BLOG POST");
-        const { id } = await req.json(); // Extract pid from the request body
+        const { id } = await req.json(); // Extract blog id from the request body
         await deleteBlog(id);
-        return NextResponse.json({ id }, { status: 200 }); // Respond with the pid
+        return NextResponse.json({ id }, { status: 200 }); // Respond with the deleted id
     } catch (err) {
         console.log(err);
         return NextResponse.json(
