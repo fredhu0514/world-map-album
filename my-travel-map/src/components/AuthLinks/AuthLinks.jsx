79abf8fd6e67e2e2e3640a7adb8646aed14060a7
Fred Hu <65211410+fredhu0514@users.noreply.github.com>
2024-01-19 01:29:11 -0500

    [New Feature]: Added Users but not Integrated (#25)

---
+++ my-travel-map/src/components/AuthLinks/AuthLinks.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import styles from "./authLinks.module.css";
+import { useSession } from "next-auth/react";
+
+export const AuthLinks = () => {
+    const { data: session, status } = useSession();
+
+    if (status === "unauthenticated") {
+        return (
+            <Link href="/login">
+                <span className={styles.loginButton}>Login</span>
+            </Link>
+        );
+    }
+
+    if (status === "authenticated" && session?.user) {
+        const profileUrl = `/profile?userId=${encodeURIComponent(
+            session?.userId
+        )}`;
+        return (
+            <div className={styles.profileContainer}>
+                <Link href={profileUrl}>
+                    <img
+                        src={session.user.image || "/default-avatar.png"}
+                        alt="Profile"
+                        className={styles.avatar}
+                    />
+                </Link>
+                {/* Additional links or dropdown can be added here */}
+            </div>
+        );
+    }
+
+    // Default placeholder for loading or undefined states
+    return <div className={styles.loadingPlaceholder}>Loading...</div>;
+};
